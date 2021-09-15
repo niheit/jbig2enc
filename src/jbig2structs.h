a36a5d09d6832adefeac5b5d4626a5a5a52fa27e
Takaaki Nihei <90757913+niheit@users.noreply.github.com>
2021-09-15 13:44:26 +0000

    Update jbig2structs.h

--- src/jbig2structs.h
+++ src/jbig2structs.h
@@ -43,15 +43,15 @@ enum {
 
 struct jbig2_file_header {
   u8 id[8];
-#ifndef _BIG_ENDIAN
+// #ifndef _BIG_ENDIAN
   u8 organisation_type : 1;
   u8 unknown_n_pages : 1;
   u8 reserved : 6;
-#else
-  u8 reserved : 6;
-  u8 unknown_n_pages : 1;
-  u8 organisation_type : 1;
-#endif
+// #else
+//  u8 reserved : 6;
+//  u8 unknown_n_pages : 1;
+//  u8 organisation_type : 1;
+// #endif
   u32 n_pages;
 } PACKED;
 
@@ -60,7 +60,7 @@ struct jbig2_page_info {
   u32 height;
   u32 xres;
   u32 yres;
-#ifndef _BIG_ENDIAN
+// #ifndef _BIG_ENDIAN
   u8 is_lossless : 1;
   u8 contains_refinements : 1;
   u8 default_pixel : 1;
@@ -68,15 +68,15 @@ struct jbig2_page_info {
   u8 aux_buffers : 1;
   u8 operator_override : 1;
   u8 reserved : 1;
-#else
-  u8 reserved : 1;
-  u8 operator_override : 1;
-  u8 aux_buffers : 1;
-  u8 default_operator : 2;
-  u8 default_pixel : 1;
-  u8 contains_refinements : 1;
-  u8 is_lossless : 1;
-#endif
+// #else
+//  u8 reserved : 1;
+//  u8 operator_override : 1;
+//  u8 aux_buffers : 1;
+//  u8 default_operator : 2;
+//  u8 default_pixel : 1;
+//  u8 contains_refinements : 1;
+//  u8 is_lossless : 1;
+// #endif
   u16 segment_flags;
 } PACKED;
 
@@ -87,17 +87,17 @@ struct jbig2_generic_region {
   u32 y;
   u8 comb_operator;
 
-#ifndef _BIG_ENDIAN
+// #ifndef _BIG_ENDIAN
   u8 mmr : 1;
   u8 gbtemplate : 2;
   u8 tpgdon : 1;
   u8 reserved : 4;
-#else
-  u8 reserved : 4;
-  u8 tpgdon : 1;
-  u8 gbtemplate : 2;
-  u8 mmr : 1;
-#endif
+// #else
+//  u8 reserved : 4;
+//  u8 tpgdon : 1;
+//  u8 gbtemplate : 2;
+//  u8 mmr : 1;
+// #endif
 
   // generic region segment here. You may not need to write all 8 bytes here.
   // If the template is 1..3 only the first two are needed.
@@ -105,7 +105,7 @@ struct jbig2_generic_region {
 } PACKED ;
 
 struct jbig2_symbol_dict {
-#ifndef _BIG_ENDIAN
+// #ifndef _BIG_ENDIAN
   u8 sdhuff:1;
   u8 sdrefagg:1;
   u8 sdhuffdh:2;
@@ -117,19 +117,19 @@ struct jbig2_symbol_dict {
   u8 sdtemplate:2;
   u8 sdrtemplate:1;
   u8 reserved:3;
-#else
-  u8 reserved:3;
-  u8 sdrtemplate:1;
-  u8 sdtemplate:2;
-  u8 bmcontextretained:1;
-  u8 bmcontext:1;
-  u8 sdhuffagginst:1;
-  u8 sdhuffbmsize:1;
-  u8 sdhuffdw:2;
-  u8 sdhuffdh:2;
-  u8 sdrefagg:1;
-  u8 sdhuff:1;
-#endif
+// #else
+//  u8 reserved:3;
+//  u8 sdrtemplate:1;
+//  u8 sdtemplate:2;
+//  u8 bmcontextretained:1;
+//  u8 bmcontext:1;
+//  u8 sdhuffagginst:1;
+//  u8 sdhuffbmsize:1;
+//  u8 sdhuffdw:2;
+//  u8 sdhuffdh:2;
+//  u8 sdrefagg:1;
+//  u8 sdhuff:1;
+// #endif
 
   signed char a1x, a1y, a2x, a2y, a3x, a3y, a4x, a4y;
 
@@ -146,7 +146,7 @@ struct jbig2_text_region {
   u32 y;
   u8 comb_operator;
 
-#ifndef _BIG_ENDIAN
+// #ifndef _BIG_ENDIAN
   u8 sbcombop2:1;
   u8 sbdefpixel:1;
   u8 sbdsoffset:5;
@@ -157,18 +157,18 @@ struct jbig2_text_region {
   u8 refcorner:2;
   u8 transposed:1;
   u8 sbcombop1:1;
-#else
-  u8 sbcombop1:1;
-  u8 transposed:1;
-  u8 refcorner:2;
-  u8 logsbstrips:2;
-  u8 sbrefine:1;
-  u8 sbhuff:1;
-  u8 sbrtemplate:1;
-  u8 sbdsoffset:5;
-  u8 sbdefpixel:1;
-  u8 sbcombop2:1;
-#endif
+// #else
+//  u8 sbcombop1:1;
+//  u8 transposed:1;
+//  u8 refcorner:2;
+//  u8 logsbstrips:2;
+//  u8 sbrefine:1;
+//  u8 sbhuff:1;
+//  u8 sbrtemplate:1;
+//  u8 sbdsoffset:5;
+//  u8 sbdefpixel:1;
+//  u8 sbcombop2:1;
+// #endif
 
   // huffman flags omitted
 } PACKED;
